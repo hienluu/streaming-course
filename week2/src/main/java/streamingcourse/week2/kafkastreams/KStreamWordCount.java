5f5985b3c3eec7ccb53a97d4dcf7a8890f4a5c75
hienluu-dd <hien.luu@doordash.com>
2022-05-24 07:23:12 -0700

    fixed the KStreamWordCount and instructions to reset

--- week2/src/main/java/streamingcourse/week2/kafkastreams/KStreamWordCount.java
+++ week2/src/main/java/streamingcourse/week2/kafkastreams/KStreamWordCount.java
@@ -1,15 +1,12 @@
 package streamingcourse.week2.kafkastreams;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.utils.Bytes;
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.*;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Materialized;
-import org.apache.kafka.streams.kstream.Produced;
-import org.apache.kafka.streams.state.KeyValueStore;
+
 
 import java.util.Arrays;
 import java.util.Properties;
@@ -18,37 +15,57 @@ import java.util.regex.Pattern;
 import static streamingcourse.week2.KafkaCommonProperties.*;
 
 public class KStreamWordCount {
-    public static final String WORD_COUNT_OUTPUT_TOPIC_NAME = "streaming-week2-wordcount-output";
+    public static final String WORD_COUNT_INPUT_TOPIC_NAME = "week2-wordcount-input";
+    public static final String WORD_COUNT_OUTPUT_TOPIC_NAME = "week2-wordcount-output";
 
     public static void main(final String[] args) throws Exception {
         System.out.println("============== KStreamWordCount.main ============= ");
-        System.out.println("reading tweets from:  " + TWEETS_TOPIC_NAME);
+        System.out.println("reading lines from:  " + WORD_COUNT_INPUT_TOPIC_NAME);
         System.out.println("writing word count results to:  " + WORD_COUNT_OUTPUT_TOPIC_NAME);
         System.out.println("============== KStreamWordCount.main ============= ");
 
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kstreams-wordcount-app");
+        props.put(StreamsConfig.CLIENT_ID_CONFIG, "kstreams-wordcount-app-client");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER_LIST);
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+        // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
+        // Note: To re-run the demo, you need to use the offset reset tool:
+        // https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Streams+Application+Reset+Tool
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // Records should be flushed every 10 seconds. This is less than the default
+        // in order to keep this example interactive.
+        props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
+        // For illustrative purposes we disable record caches.
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
 
         StreamsBuilder builder = new StreamsBuilder();
-        // create a stream from the TWEETS_TOPIC_NAME
-        KStream<String, String> tweets = builder.stream(TWEETS_TOPIC_NAME);
+        // create a stream from the WORD_COUNT_INPUT_TOPIC_NAME
+        KStream<String, String> lineStream = builder.stream(WORD_COUNT_INPUT_TOPIC_NAME);
 
-        // regex pattern to split each tweet into words
+        // regex pattern to split each line into words
         Pattern pattern = Pattern.compile("\\W+", Pattern.UNICODE_CHARACTER_CLASS);
 
-        KTable<String, Long> wordCounts = tweets
-                .flatMapValues(tweet -> Arrays.asList(pattern.split(tweet.toLowerCase())))
+        KTable<String, String> wordCounts = lineStream
+                .flatMapValues(line -> Arrays.asList(pattern.split(line.toLowerCase())))
+                //.map((key,value) -> new KeyValue<Object, Object>(value, value))
+                .filter((key,value) -> !value.equals("the"))
                 .groupBy((key, word) -> word)
-                .count();
+                .count()
+                        .mapValues(value -> Long.toString(value));
 
-        wordCounts.toStream().to(WORD_COUNT_OUTPUT_TOPIC_NAME, Produced.with(Serdes.String(), Serdes.Long()));
+        wordCounts.toStream().to(WORD_COUNT_OUTPUT_TOPIC_NAME);
 
-        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        System.out.println("Building topology");
+        Topology topology = builder.build();
+        System.out.println("topology: "  + topology.describe().toString());
+        KafkaStreams streams = new KafkaStreams(topology, props);
 
         // Now run the processing topology via `start()` to begin processing its input data.
+        System.out.println("Start running the topology");
         streams.start();
 
         // shutdown hook to properly and gracefully close the streams application
